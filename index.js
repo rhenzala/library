857db66a8c4ad94f9a582a1e0188c6d93740bc50
rhenzala <97076128+rhenzala@users.noreply.github.com>
2023-09-16 15:41:49 +0800

    Add function that display the book listed in myLibrary

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+const container = document.querySelector('.card-container');
+const addBookBtn = document.getElementById('addBook');
+
 const myLibrary = [];
 
 function Book(title, author, pages, isRead){
@@ -15,10 +18,48 @@ function addBookToLibrary(title, author, pages, isRead){
 
     myLibrary.push(book);
 }
+addBookToLibrary("How To Not", "Someone", 78, true)
+addBookToLibrary("Lorem Ipsum", "Lorem", 842, true)
+addBookToLibrary("How I created Linux", "Linus Tech Tips", 690, false)
+addBookToLibrary("brrrrt brrrt", "A-10", 420, true)
 
 function displayBook(){
-    
+    const card = document.createElement('div');
+    const cardTitle = document.createElement('div');
+    const cardAuthor = document.createElement('div');
+    const cardPages = document.createElement('div');
+    const cardToggle = document.createElement('button');
+    const cardDelete = document.createElement('button');
+
+    card.classList.add('card');
+    container.appendChild(card);
+
+    for (let book of myLibrary){
+        let status = (book.isRead) ? "READ": "NOT READ";
+        
+        cardTitle.classList.add('title')
+        cardTitle.textContent = `Title: ${book.title}`;
+        card.appendChild(cardTitle);
+
+        cardAuthor.classList.add('author')
+        cardAuthor.textContent = `Author: ${book.author}`;
+        card.appendChild(cardAuthor);
+
+        cardPages.classList.add('pages')
+        cardPages.textContent = `Pages: ${book.pages}`;
+        card.appendChild(cardPages);
+
+        cardToggle.classList.add('toggle-button');
+        cardToggle.textContent = `${status}`;
+        card.appendChild(cardToggle);
+
+        cardDelete.classList.add('delete')
+        cardDelete.textContent = `Delete`;
+        card.appendChild(cardDelete);
+    }
 }
 
-addBookToLibrary("nigger", "retard", 78, false)
-console.log(myLibrary)
+displayBook();
+
+
+
